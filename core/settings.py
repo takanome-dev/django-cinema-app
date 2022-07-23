b7fd5420949821d2296a465eca15439d17a9f600
takanome_dev <takanomedev221@gmail.com>
2022-07-23 19:10:17 +0000

    refactor: update settings

--- core/settings.py
+++ core/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 SITE_ID = 1
 
-# Default primary key field type
+# Default primary key field type 
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
