20a85fe0c94380e63d745bc40303014e148ed4e0
takanome_dev <takanomedev221@gmail.com>
2022-07-24 16:07:43 +0000

    refactor: update release script

--- release.sh
+++ release.sh
@@ -2,5 +2,5 @@ echo "⚡ Migrating DB..."
 python3 manage.py migrate
 
 echo "🌱 Loading fixtures..."
-python3 manage.py loaddata movies/fixtures/movies.json
 python3 manage.py loaddata movies/fixtures/genres.json
+python3 manage.py loaddata movies/fixtures/movies.json
